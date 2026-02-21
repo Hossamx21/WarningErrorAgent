afaa8e5e034ef35dd65f3ba98d03dcaf014f3261
HossamHassan <hossam.hassan@garraio.com>
2026-02-21 20:01:34 +0200

    updating test

---
+++ testcode/math_utils.h
@@ -0,0 +1,6 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int add_numbers(int a, int b);
+
+#endif // MATH_UTILS_H
--- testcode/test.c
+++ testcode/test.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math_utils.h"
 
 // WARNING 1: Unused parameter
-// We pass 'config_id' into the function, but never actually use it inside.
 void init_sensor(int config_id) {
     printf("Sensor initialized.\n");
 }
 
 int main() {
     // WARNING 2: Unused variable
-    // We declare a variable and give it a hex value, but never read it later.
     int diagnostic_code = 0xFF; 
 
     int x = 50;
-    int sum = x+10;
+    int sum = add_numbers(10, x);
     
     init_sensor(1);
     printf("System ready. Sum: %d\n", sum);
