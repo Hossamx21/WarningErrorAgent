fe2f648fdabcbf88a0a4f0cb3f40940e449c5432
HossamHassan <hossam.hassan@garraio.com>
2026-03-01 16:28:52 +0200

    ai fix

--- testcode/test.c
+++ testcode/test.c
@@ -3,7 +3,6 @@
 #include "math_utils.h"
 // A weird GCC specific directive that throws an obscure warning
 #pragma GCC diagnostic warning "-Wunknown-pragmas"
-#pragma this_is_a_fake_pragma_to_confuse_the_compiler
 // WARNING 1: Unused parameter
 void init_sensor(int config_id) {
     printf("Sensor initialized.\n");
